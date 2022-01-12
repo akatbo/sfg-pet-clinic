da20ccbe4dd22d1c80a812137759e4ba2c8a2b1a
akatbo <akatbo@gmail.com>
2022-01-12 11:18:54 +0200

    adding POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firtName;
+    private String lstName;
+
+    public String getFirtName() {
+        return firtName;
+    }
+
+    public void setFirtName(String firtName) {
+        this.firtName = firtName;
+    }
+
+    public String getLstName() {
+        return lstName;
+    }
+
+    public void setLstName(String lstName) {
+        this.lstName = lstName;
+    }
+}
